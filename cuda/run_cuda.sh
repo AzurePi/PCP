d77bc10d6620de4a35d61801ae323cff709a2aab
azure <ppcardana@hotmail.com>
2024-11-22 22:57:52 -0300

    CUDA (ainda com erros)

---
+++ cuda/run_cuda.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Define variáveis
+BUILD_DIR="build"
+EXECUTABLE="./cuda"  # Caminho para o executável gerado
+
+# Remove diretório de build anterior (se existir) e cria um novo
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+
+# Compila o código com CMake
+cmake ..
+make
+
+# Volta ao diretório raiz
+cd ..
+
+# Define os parâmetros de teste
+BLOCKS_LIST=(10 100 1000)          # Lista de números de blocos
+INTERVALS_LIST=(1000 10000 100000) # Lista de intervalos
+
+# Loop para testar todas as combinações de parâmetros
+for BLOCKS in "${BLOCKS_LIST[@]}"; do
+  for X_INTERVALS in "${INTERVALS_LIST[@]}"; do
+    for Y_INTERVALS in "${INTERVALS_LIST[@]}"; do
+      echo "Executando com $BLOCKS bloco(s), X=$X_INTERVALS, Y=$Y_INTERVALS"
+      $EXECUTABLE "$X_INTERVALS" "$Y_INTERVALS" "$BLOCKS"
+    done
+  done
+done
+
+echo "Todas as execuções foram concluídas. Resultados salvos em tempos_CUDA.txt"
